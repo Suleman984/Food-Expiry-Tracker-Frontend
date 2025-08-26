57cae93b08f68cd00fe02b9ea9b16a18220a2728
msuleman984 <msulemn4@gmail.com>
2025-08-26 22:00:43 +0500

    Updated SupabaseClient

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
-    "Missing environment variables. Please set NEXT_PUBLIC_SUPABASE_URL and SUPABASE_ANON_KEY.!"
+    "Missing environment variables. Please set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY."
   );
 }
 

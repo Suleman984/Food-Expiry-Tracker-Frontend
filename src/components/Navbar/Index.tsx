3a59aec356b014fb066c969468b58bc6298a0e61
msuleman984 <msulemn4@gmail.com>
2025-08-27 10:52:14 +0500

    Updated Navbar Styles

--- src/components/Navbar/Index.tsx
+++ src/components/Navbar/Index.tsx
@@ -4,10 +4,12 @@ import { AuthButton } from "../AuthButton";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white p-4 flex justify-between items-center  shadow-lg">
-      <Link href="/" className="text-xl font-bold">
+    <nav className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white p-4 max-md:flex max-md:flex-col max-md:items-start flex justify-between items-center shadow-lg">
+      <div>
+        <Link href="/" className="text-xl font-bold">
         🍎 Food Tracker
       </Link>
+      </div>
       <div className="flex gap-4 items-center">
         <Link href="/dashboard">Dashboard</Link>
         <AuthButton />

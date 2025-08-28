1a698bff2a6fc9ee471a99e36884af187da6a740
msuleman984 <msulemn4@gmail.com>
2025-08-28 23:09:27 +0500

    removed error from login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -196,7 +196,7 @@ export default function LoginPage() {
           
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600 dark:text-gray-400">
-              Don't have an account?{" "}
+              Don&apos;t have an account?{" "}
               <button
                 onClick={() => router.push("/signup")}
                 disabled={isLoading}
